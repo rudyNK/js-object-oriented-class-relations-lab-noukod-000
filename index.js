2a4d0ff4f5f17beeaa900e859be99566a62724a1
Rudy Ravelin <rudy@noukod.com>
2019-03-13 06:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-class User {
-  constructor(name, email) {
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
     this.name = name;
-    this.email = email;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
-
-  sayHello() {
-    console.log(`Hello, my name is ${this.name}`);
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
   }
 }
 
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
+  }
 
-class Teacher extends User {
-  sayHello(){
-    super.sayHello()
-    console.log('hello')
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
   }
 }
 
-rudy.sayHello()
-// Hello, my name is rudy
-// hello
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
