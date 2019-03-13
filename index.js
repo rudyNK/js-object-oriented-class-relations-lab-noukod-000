ce3b77fdfe0c05a721dcd00611d5a766b0497099
Rudy Ravelin <rudy@noukod.com>
2019-03-13 06:07:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+
+class Teacher extends User {
+  sayHello(){
+    super.sayHello()
+    console.log('hello')
+  }
+}
+
+rudy.sayHello()
+// Hello, my name is rudy
+// hello
